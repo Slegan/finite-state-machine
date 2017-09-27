b44094dd050aa5f09c023f35d3a75ca72ab8f828
Poluyan Sasha <Sleganlegat@gmail.com>
2017-09-27 13:00:17 +0300

    solution

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,59 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      if (config === null) {
+        throw new Error();
+      }else {
+        this.state = config;
+        this.state.currentState = this.state.initial;
+        this.state.history = [this.state.initial];
+      }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.state.currentState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      let stateArr = Object.keys(this.state.states);
+      if (!stateArr.includes(state)) {
+        throw new Error();
+      }else {
+        this.state.currentState = state;
+        this.state.history.push(state);
+      }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (!this.state.states[this.state.currentState].transitions[event]) {
+        throw new Error();
+      }else if (this.state.states[this.state.currentState].transitions[event]) {
+        this.state.currentState = this.state.states[this.state.currentState].transitions[event];
+        if (this.state.currentState !== this.state.history[this.state.history.length-1]) {
+          this.state.history.push(this.state.currentState);
+        }
+      }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.state.currentState = this.state.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +63,56 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      let result = [];
+      if (event == null) { 
+        return Object.keys(this.state.states);
+      }else {
+        for (var i = 0; i < Object.keys(this.state.states).length; i++) {
+          let verifiable = Object.keys(this.state.states)[i];
+          if (Object.keys(this.state.states[verifiable].transitions).includes(event)) {
+            result.push(verifiable);
+          }
+        }
+      }
+      return result;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this.state.currentState === this.state.history[0]) {
+          return false;
+      }else if (this.state.history[this.state.history.length-2]) {
+          this.state.currentState = this.state.history[this.state.history.length-2];
+          return true;
+      }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if (this.state.currentState === this.state.history[this.state.history.length-1]) {
+        return false;
+      }else if (this.state.history[this.state.history.length-1]) {
+        this.state.currentState = this.state.history[this.state.history.length-1];
+        return true;
+      }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+      this.state.history = [this.state.initial];
+      this.state.currentState = this.state.initial;
+    }
 }
 
 module.exports = FSM;
